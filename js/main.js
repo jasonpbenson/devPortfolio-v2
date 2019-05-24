bba0666dbca30011cd53aecdd7306c2b90d9c8c6
jasonpbenson <jasonbenson3rd@gmail.com>
2019-05-23 21:10:01 -0400

    additional round of play with svg animation and some initial mobile styling

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// href="https://github.com/jasonpbenson" target="_blank"
+// href="https://www.linkedin.com/in/jason-benson-960861180/" target="_blank"
+// href="./files/JB_DevResume_2019.pdf" target="_blank"
+
+const contentModal = document.getElementById("modal");
+const aboutText = document.createElement('p');
+const aboutIcons = document.createElement('div');
