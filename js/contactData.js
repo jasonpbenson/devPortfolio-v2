cd5ec7c606badfe384afc0d025fb7f884918652a
jasonpbenson <jasonbenson3rd@gmail.com>
2019-06-11 08:49:44 -0400

    silly typo

--- js/contactData.js
+++ js/contactData.js
@@ -1,7 +1,7 @@
 let contactInfo = [
     {
         links: ["https://github.com/jasonpbenson", "https://www.linkedin.com/in/jason-benson-960861180/", "files/JB_DevResume_2019.pdf"],
-        titles: ["github", "linkdin", "resume"],
+        titles: ["github", "linkedin", "resume"],
         info: ["jasonbenson3rd@gmail.com", "410.812.4303"]
     },
 ]
